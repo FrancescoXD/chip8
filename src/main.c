5569405d963899b1b62eb6966f16923fc3f5548e
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2023-07-31 19:30:32 +0200

    added delay option

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <chip8.h>
 
 int main(int argc, char **argv) {
-	if (argc < 3) {
-		fprintf(stderr, "Usage:\n%s <SCALE> <ROM>\n\n", argv[0]);
-		fprintf(stderr, "Example:\n%s 10 roms/IBM_Logo.ch8\n", argv[0]);
+	if (argc < 4) {
+		fprintf(stderr, "Usage:\n%s <SCALE> <DELAY> <ROM>\n\n", argv[0]);
+		fprintf(stderr, "Example:\n%s 10 1000 roms/IBM_Logo.ch8\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -12,7 +12,8 @@ int main(int argc, char **argv) {
 	// Init the CHIP-8
 	Chip8_t chip8 = chip8_init();
 	uint8_t scale = atoi(argv[1]);
-	const char *filename = argv[2];
+	int delay = atoi(argv[2]);
+	const char *filename = argv[3];
 
 	fprintf(stdout, "Loading %s...\n", filename);
 	if (!chip8_load_rom(&chip8, filename)) {
@@ -32,7 +33,7 @@ int main(int argc, char **argv) {
 		quit = chip8_gui_handle_input(&chip8.keypad);
 		chip8_execute(&chip8);
 		chip8_gui_update_screen(chip8.video, sizeof(chip8.video[0]) * VIDEO_WIDTH, &gui);
-		usleep(500);
+		usleep(delay);
 	}
 	fprintf(stdout, "Closing Chip-8 emulator...\n");
 
